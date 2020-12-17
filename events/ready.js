08c320f2f225e1cad37bc1257c69bdb77d96801b
uguralp1471 <52300861+uguralp1471@users.noreply.github.com>
2020-12-17 12:14:27 +0300

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,12 @@
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Tüm komutlar çalıştırılıyor!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı! BOT aktif!`);
+  client.user.setStatus("online");
+}
